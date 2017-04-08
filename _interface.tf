0118d47c0c94a3088b3128c9ed97a9c0decc7a41
Phil Watts <phil@hashicorp.com>
2017-04-07 22:52:28 -0400

    updated output

--- _interface.tf
+++ _interface.tf
@@ -1,7 +1,6 @@
 variable "dns_hostnames" { default = true }
 variable "dns_support" { default = true }
 variable "flow_log_traffic_type" { default = "ALL" }
-variable "flowlogrole" {} # Imported from the tfmod-aws-acct module
 variable "map_public_ip_on_launch" { default = true }
 variable "network_name" {}
 variable "region" {}
@@ -11,5 +10,5 @@ variable "vpc_cidr_block" {}
 
 output "priv_subnets" { value = ["${aws_subnet.priv.*.id}"] }
 output "pub_subnets" { value = ["${aws_subnet.pub.*.id}"] }
-output "vpc_id" { value = ["${aws_vpc.vpc.id}"] }
-output "priv_route_table_ids" { value = "${aws_route_table.priv.*.id}" }
+output "vpc_id" { value = "${aws_vpc.vpc.id}" }
+output "priv_route_table_ids" { value = ["${aws_route_table.priv.*.id}"] }
