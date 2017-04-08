4114a9c62941bc1ec51ec22c7e2edd9c9d17721c
Phil Watts <phil@hashicorp.com>
2017-04-07 22:55:00 -0400

    updated output

--- _interface.tf
+++ _interface.tf
@@ -1,6 +1,7 @@
 variable "dns_hostnames" { default = true }
 variable "dns_support" { default = true }
 variable "flow_log_traffic_type" { default = "ALL" }
+variable "flowlogrole" {} # Imported from the tfmod-aws-acct module
 variable "map_public_ip_on_launch" { default = true }
 variable "network_name" {}
 variable "region" {}
